2a8edc345d490bf62cf032a78500ce3a975cb077
Dolon Roy <dolonr718@gmail.com>
2023-12-25 16:09:54 +0600

    topic add functionality added commit

--- src/pages/AddCourse/AddCourse.jsx
+++ src/pages/AddCourse/AddCourse.jsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const AddCourse = () => {
+    const [topics, setTopics] = useState([]);
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm();
 
+    const handleAddTopic = () => {
+        const topic = document.querySelector("#topic").value;
+        document.querySelector("#topic").value = "";
+        setTopics([...topics, topic]);
+    }
+
+    const handleRemoveTopics = (idx) => {
+       topics.splice(idx, 1);
+       setTopics([...topics]);
+    }
 
     return (
         <div>
             <h1 className="md:text-5xl text-3xl font-bold text-center py-10">Add Course</h1>
 
             <section className="p-6 ">
-                <form noValidate="" action="" className="container flex flex-col mx-auto space-y-12">
+                <form onSubmit={handleSubmit()} noValidate="" action="" className="container flex flex-col mx-auto space-y-12">
                     <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm ">
                         <div className="space-y-2 col-span-full lg:col-span-1">
                             <p className="font-medium">Course Information</p>
@@ -25,49 +37,66 @@ const AddCourse = () => {
                                 <input {...register("name", { required: true })} id="name" type="text" placeholder="Course Name" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
                                 {errors.name && <span className="text-red-500 md:text-sm text-xs pt-1">This field is required</span>}
                             </div>
-                            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                                <div className="col-span-full sm:col-span-3">
-                                    <label htmlFor="price" className="text-sm">Price (BDT)</label>
-                                    <input {...register("price", { required: true })} id="price" type="text" placeholder="Course Price" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
-                                    {errors.price && <span className="text-red-500 md:text-sm text-xs pt-1">This field is required</span>}
-                                </div>
-                            </div>
-                            <div className="col-span-full">
-                                <label htmlFor="bio" className="text-sm">Bio</label>
-                                <input id="bio" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 "></input>
-                            </div>
+
                             <div className="col-span-full sm:col-span-3">
-                                <label htmlFor="email" className="text-sm">Email</label>
-                                <input id="email" type="email" placeholder="Email" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+                                <label htmlFor="price" className="text-sm">Price (BDT)</label>
+                                <input {...register("price", { required: true })} id="price" type="text" placeholder="Course Price" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+                                {errors.price && <span className="text-red-500 md:text-sm text-xs pt-1">This field is required</span>}
                             </div>
+
                             <div className="col-span-full">
-                                <label htmlFor="address" className="text-sm">Address</label>
-                                <input id="address" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+                                <label htmlFor="bio" className="text-sm">Description</label>
+                                <textarea {...register("description", { required: true })} id="description" placeholder="Course Description" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 "></textarea>
+                                {errors.description && <span className="text-red-500 md:text-sm text-xs pt-1">This field is required</span>}
+                            </div>
+
+                            <div className="col-span-full sm:col-span-2">
+                                <label htmlFor="duration" className="text-sm">Duration (Weeks)</label>
+                                <input {...register("duration", { required: true })} id="duration" type="number" placeholder="Course duration" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+                                {errors.duration && <span className="text-red-500 md:text-sm text-xs pt-1">This field is required</span>}
                             </div>
                             <div className="col-span-full sm:col-span-2">
-                                <label htmlFor="city" className="text-sm">City</label>
-                                <input id="city" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+                                <label htmlFor="level" className="text-sm">Level</label>
+                                <select {...register("level", { required: true })} className="select select-bordered w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 ">
+                                    <option disabled selected>Choose Level</option>
+                                    <option value={"Beginner"}>Beginner</option>
+                                    <option value={"Mid Level"}>Mid Level</option>
+                                    <option value={"Expertise"}>Expertise</option>
+                                </select>
+                                {errors.level && <span className="text-red-500 md:text-sm text-xs pt-1">This field is required</span>}
                             </div>
                             <div className="col-span-full sm:col-span-2">
-                                <label htmlFor="state" className="text-sm">State / Province</label>
-                                <input id="state" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+                                <label htmlFor="class-time" className="text-sm">Class Time</label>
+                                <input {...register("class_time", { required: true })} id="class-time" type="text" placeholder="Course class time" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+                                {errors.class_time && <span className="text-red-500 md:text-sm text-xs pt-1">This field is required</span>}
                             </div>
-                            <div className="col-span-full sm:col-span-2">
-                                <label htmlFor="zip" className="text-sm">ZIP / Postal</label>
-                                <input id="zip" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
+
+                            <div className="col-span-full sm:col-span-3">
+                                <label htmlFor="topic" className="text-sm">Topics</label>
+                                <textarea id="topic" type="text" placeholder="Course Name" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200 p-5 flex items-start h-24" />
+                                <span onClick={handleAddTopic} className="md:btn-sm mt-4 btn btn-xs md:btn-primary btn-primary md:text-sm text-xs">Add Topic</span>
                             </div>
-                            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                                <div className="col-span-full sm:col-span-3">
-                                    <label htmlFor="username" className="text-sm">Username</label>
-                                    <input id="username" type="text" placeholder="Username" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
-                                </div>
-                                <div className="col-span-full sm:col-span-3">
-                                    <label htmlFor="website" className="text-sm">Website</label>
-                                    <input id="website" type="text" placeholder="https://" className="w-full rounded-md focus:ring focus:ri focus:ri bg-gray-200  p-3 " />
-                                </div>
+                            <div className="col-span-full sm:col-span-3 md:pt-5">
+                                {
+                                    topics?.length > 0 ? <div className="flex md:gap-5 gap-3 flex-wrap">
+                                        <h1>{topics?.length}</h1>
+                                            {
+                                                topics?.map((item, idx) => <div className="bg-gray-500 p-2 pl-5 pr-8 rounded-lg relative text-white" key={idx}>
+                                                    <h1 className="md:text-base text-sm font-semibold">
+                                                        {item}
+                                                    </h1>
+                                                    <span onClick={() => handleRemoveTopics(idx)} className="btn btn-xs btn-error absolute top-0 right-0">x</span>
+                                                </div>)
+                                            }
+                                    </div> : 
+                                    <h5 className="md:text-sm text-xs text-red-500">No topics available</h5>
+                                }
                             </div>
                         </div>
                     </fieldset>
+                    <div>
+                        <button type="submit">Add Course</button>
+                    </div>
                 </form>
             </section>
         </div>
