67be53b5a92b46398aca1cdbd1ff31d8a20c400a
Dolon Roy <dolonr718@gmail.com>
2023-12-25 12:32:50 +0600

    successfully added

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import{
 RouterProvider,
 } from "react-router-dom";
 import MainRouter from './router/MainRouter';
+import AuthProvider from './Provider/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={MainRouter}/>
+    <AuthProvider>
+      <RouterProvider router={MainRouter}/>
+    </AuthProvider>
   </React.StrictMode>,
 )
